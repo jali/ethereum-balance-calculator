88052688d25929d4184680123a6bccb11232543c
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2016-12-23 00:53:31 +0000

    final

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-var express = require('express');
+const express = require('express'),
+	async = require('asyncawait/async'),
+	await = require('asyncawait/await'),
+	axios = require('axios'),
+	bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
 
-
+var calculate = (a) => { return a/=Math.pow(10, 18) }
 
 //configuration
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res){
 	res.render('index', {
-		pageTitle:'Simple Express 3 and Jade example'
+		pageTitle:'ES6 NodeJS app that uses async/await!',
 	});
 });
 
-var server = app.listen(3010);
+app.post('/', function(req, res){
+	let postedData = req.body;
+	
+	// call api
+	let url = `https://api.blockcypher.com/v1/eth/main/addrs/${postedData.address}/balance`
 
-console.log('Example app listening at http://localhost:3010');
+	const getEthereumAsync = async ( () => {
+	    let result = await (axios(url))
+	    let d = await (result.data);
+	    return d;
+		});
+
+	getEthereumAsync()
+		.then(d => res.send(`<p>Ethereum address: ${postedData.address}</p>
+		Ethereum balance calculated: ${calculate(d.balance)}`))
+		// .then(d => console.log(calculate(d.balance)))
+		.catch(reason => console.log(reason));
+});
+
+// run server
+const server = app.listen(3000);
+console.log('App listening at http://localhost:3000');
+
